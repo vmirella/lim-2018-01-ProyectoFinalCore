49c9b221c6d4b263f4be9519a04a7f1984751b40
unknown <vmirella_17@hotmail.com>
2018-08-05 22:55:29 -0500

    se creo modelos de como seran las funciones

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,3 @@
-// Initialize Firebase
 var config = {
 	apiKey: "AIzaSyAt-lvHjfjUVd0Urz1i1JxbmnjSkjrJw4E",
 	authDomain: "visitame-8b1e9.firebaseapp.com",
@@ -8,3 +7,24 @@ var config = {
 	messagingSenderId: "658184057823"
 };
 firebase.initializeApp(config);
+
+//Ejemplo de como llamar la funcion
+getClients()
+.then((snapshot) => {
+	console.log(snapshot.val());
+	//imprimir los clientes en el sitio deseado
+})
+.catch((error) => {
+	console.log(error);
+});
+
+getEmployees('LJC5kBGvC-BBmDKVnQa')
+.then((snapshot) => {
+	console.log(snapshot.val());
+	//imprimir los clientes en el sitio deseado
+})
+.catch((error) => {
+	console.log(error);
+});
+
+//createEmployees();
